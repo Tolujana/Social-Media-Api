894c205f4f52b7f61687a0f4837d8822e32c03da
Tolu Olofinjana <Tolujana@gmail.com>
2022-03-29 09:47:13 +0100

    updated auth.js, users.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcryptjs');
 
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -7,17 +8,19 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const userRoute = require('./route/users');
 const authRoute = require('./route/auth');
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB, () => {
-  console.log('we are working');
+mongoose.connect(process.env.MONGO_DB, (err) => {
+  if (err) console.log(err);
+  else console.log('mongdb is connected');
 });
 // middlewere check this
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
-app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen(8800, () => console.log('server runnings '));
 
--- route/users.js
+++ route/users.js
@@ -1,7 +1,38 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User');
 
-router.get("/", (req, res) => {
-  res.send("did it");
+//get a user
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    console.log(user);
+    const { password, updatedAt, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+//update user
+router.put('/:id/update', async (req, res) => {
+  if (req.params.id === req.body.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcryptjs.genSalt(10);
+        req.body.password = await bcryptjs.hash(req.body.password, salt);
+      } catch (e) {
+        e.message;
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json(user);
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
 });
 
 module.exports = router;
